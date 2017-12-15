b816b002f6d363766e1a07a4b1b6329468476d99
lijiawei0418 <jiaweil3@illinois.edu>
2017-12-14 20:19:19 -0600

    Final version of the parking simulation system.

--- Test.py
+++ Test.py
@@ -1,28 +1,35 @@
 import numpy as np
 import pandas as pd
-from datetime import datetime
-# Module-level Variable
+import os
 from scipy.stats import norm
 
+# Module-level Variable
 # unit checking time for the system
 UNIT_TIME = 1
 # Parameter for Pert distribution
 MODIFIED_LAMBDA = 4
 # Each array represents a street. The length of array represents the number of meters on that street, and the item in the array represents the time that the car will stoped.
-DANIELSTREET = np.zeros(23)
-SIXTHSTREET = np.zeros(30)
-CHALMERSSTREET = np.zeros(23)
-FIFTHSTREET = np.zeros(10)
-STREET = np.zeros(4)
+DANIELSTREET = (np.zeros(13)).astype(int)
+SIXTHSTREET = (np.zeros(20)).astype(int)
+CHALMERSSTREET = (np.zeros(13)).astype(int)
+FIFTHSTREET = (np.zeros(5)).astype(int)
+STREET = (np.zeros(4)).astype(int)
 # whole parking lots around iSchool
 PARKINGLOTSMAPPING = {0:'DANIELSTREET', 1: 'SIXTHSTREET', 2: 'CHALMERSSTREET', 3: 'FIFTHSTREET', 4: 'STREET'}
 PARKINGLOTS = np.array([DANIELSTREET, SIXTHSTREET, CHALMERSSTREET, FIFTHSTREET, STREET])
+WEEKDAY = ('Monday', 'Tuesday', 'Wednesday','Thursday', 'Friday')
+
+# Data for doctest
 DANIELSTREET_T = np.array([0, 0, 0])
 SIXTHSTREET_T = np.array([0, 0, 0])
 PARKINGLOTS_TEST = np.array([DANIELSTREET_T, SIXTHSTREET_T])
 
 
+
 class RandomDurationTimeGenerator():
+
+    # This class is used to generate the random duration time for all car coming in.
+
     def CarStayDuration(self, sample_number, low=1, mode=3, high=4):
         """
         This function is to produce random numbers according to the 'Modified PERT' distribution.
@@ -47,22 +54,36 @@ class RandomDurationTimeGenerator():
         return duration_m
 
     def ErrorStayDuration(self, sample_number):
+        '''
+        This function is used to generate an array of duration time for car not go for class. Its distribution is uniform distribution.
+        :param sampleNumber: The number of the random sample. This would be defined by our parking system.
+        :return: a numpy array with random numbers that fit a uniform distribution
+        '''
         car_stay_error = (np.random.uniform(20, 61, size = sample_number)).astype(int)
         return car_stay_error
 
 
 class RandomCarInGenerator():
+
+    # This class is used to generate random number of car comming into the parking lots.
+
     def __init__(self, week: str,  hour:int, minute:int):
         self.hour = hour
         self.minute = minute
         self.week = week
 
     def PersonRegistered(self, dataFile):
+        '''
+        Get the hour index of the data file
+        :param dataFile: file's name
+        :return: a series of hours in the data file
+        '''
         number = pd.read_csv(dataFile, index_col='hour')
         return number
 
     def PersonIn(self):
         """
+        Generate a uniform distribution of a percentage that a person attends in the class
         :return: (distribution)number of person attending in the class
         """
         person_uniform = np.random.uniform(0.5, 0.9)
@@ -71,6 +92,7 @@ class RandomCarInGenerator():
 
     def CarInPercent(self):
         """
+        Generate a uniform distribution of a percentage that person have cars
         :return: percentage of person who drive car and stop the car in the parking area
         """
         car_uniform = np.random.uniform(0.3, 0.5)
@@ -79,6 +101,7 @@ class RandomCarInGenerator():
 
     def CarInDist(self):
         """
+        This function is used to generate the number of car that will come into the parking lots at each unit time.
         :param time: checking time
         :return: car in distribution around starting time of class
         >>> RandomCarInGenerator.CarInDist(RandomCarInGenerator)
@@ -110,20 +133,20 @@ class RandomCarInGenerator():
 
     def CarInError(self, sample_number):
         """
+        Number of car in the parking lots at each unit time which are not for the class.
         :param time: checking time
         :return: Error number of Car (uniform_distribution)
         """
         car_in_error = (np.random.uniform(-2, 2, size = sample_number)).astype(int)
         e = car_in_error < 0
         car_in_error[e] = 0
-
         return car_in_error
 
 
-# print(RandomCarInGenerator("Monday", 9, 50).CarInDist())
-# print(car.CarInDist())
-
 class ParkingLot():
+
+    # This class is used to find an available meter for a car
+
 
     def FindParkingMeter(self, parking_status: np.array):
         '''
@@ -146,6 +169,8 @@ class ParkingLot():
             return None
 
 class ParkingSystem():
+
+    # This class is used to assign a car to a parking slot
 
     def UnitTimeCheck(self, parking_status: np.array):
         '''
@@ -231,26 +256,46 @@ class ParkingSystem():
 
 class iSchoolParkingSimulator():
 
-    def __init__(self):
+    def __init__(self, parking_status_original):
         self.input_hours = 0
         self.input_minutes = 0
         # The system will start from 8:00
         self.start = 8
         self.parking_status = 0
         self.week = ''
+        self.parking_satus_original = parking_status_original
 
     def Main(self):
         '''
-
-        :return:
-        >>> iSchoolParkingSimulator.Main(iSchoolParkingSimulator)
+        This function is used to start the parking simulation process.
         '''
-        require_week = input("Please enter the day you want to check (Monday ~ Friday):")
+
+        # User enter the weekday he/she wants to query
+        while(1):
+            require_week = input("Please enter the day you want to check (Monday ~ Friday):")
+            if require_week not in WEEKDAY:
+                print("Please enter correct weekday.")
+            else:
+                break
         self.week = require_week
-        require_time = input("Please enter the time you want to check (e.g.: 8:20):")
-        require_time = require_time.split(':')
-        self.input_hours = int(require_time[0])
-        self.input_minutes = int(require_time[1])
+
+        while(1):
+            require_time = input("Please enter the time you want to check (e.g.: 8:20) from 8:00 to 17:59:")
+            try:
+                require_time = require_time.split(':')
+                self.input_hours = int(require_time[0])
+                self.input_minutes = int(require_time[1])
+            except:
+                print("Please enter correct time.")
+            if (self.input_hours > 17) | (self.input_hours < 8):
+                print("Please enter hour range from 8:00 to 17:00.")
+                continue
+            if (self.input_minutes > 60) | (self.input_minutes < 0):
+                print("Please enter minutes range from 00 to 60.")
+            else:
+                break
+
+
         # Calculate the times to operate the UNIT simulation process
         simulation_times = int(((self.input_hours - 8) * 60 + self.input_minutes) / UNIT_TIME)
 
@@ -266,7 +311,7 @@ class iSchoolParkingSimulator():
             print("This is the " + str(i + 1) + " time simulation the parking process.")
             hour = self.start
             minute = 0
-            self.parking_status = PARKINGLOTS_TEST
+            self.parking_status = self.parking_satus_original
             # Simulate the process of parking
             for j in range(simulation_times):
                 print("----------------------------------------------------")
@@ -292,16 +337,12 @@ class iSchoolParkingSimulator():
         print("After unit checking:")
         print(self.parking_status)
 
-
         # The number of error car coming into the parking lot at the unit time
         print("car in:")
         print(car_in_number)
         print("error car in:")
         print(car_error_number)
 
-        #test:
-        # car_error_number = 1
-        # car_in_number = 1
         r_d = RandomDurationTimeGenerator()
         if car_in_number != 0:
             # A list of duration for car to stop
@@ -326,7 +367,14 @@ class iSchoolParkingSimulator():
             print(self.parking_status)
 
 
-s = iSchoolParkingSimulator()
+
+# delete the original record file
+try:
+    os.remove('EmptyRecords.csv')
+except OSError:
+    pass
+
+s = iSchoolParkingSimulator(PARKINGLOTS)
 s.Main()
 # Calculate probability of parking at each street
 print("\n\n**************************************************")
